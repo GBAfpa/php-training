41e8087fdee6f643d4cf1509b8a23eca4044af54
Guillaume Afpa <guillaume.belleuvre@afpa.fr>
2022-09-05 14:47:14 +0200

    exo5 timestamp on styles.css

--- exo5.php
+++ exo5.php
@@ -20,7 +20,7 @@ function getHtmlFromArray(array $array) :string {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/global.css">
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/styles.css?<?=time()?>">
     <title>Introduction PHP - Exo 5</title>
 </head>
 <body class="dark-template">
